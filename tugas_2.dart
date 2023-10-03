179c771fe1f844f7a3406a6509297b035006e06e
hafizhpebrian <hafizhpebrian_2226250077@mhs.mdp.ac.id>
2023-10-03 14:19:39 +0700

    tugas 1.2

---
+++ tugas_2.dart
@@ -0,0 +1,16 @@
+import 'persegiPanjang.dart';
+
+void main() {
+  int panjang = 5;
+  int lebar = 3;
+
+  PersegiPanjang persegi = PersegiPanjang(panjang, lebar);
+
+  int luas = persegi.hitungLuas();
+  int keliling = persegi.hitungKeliling();
+
+  print('Panjang Persegi Panjang : $panjang Lebar Persegi Panjang : $lebar');
+  print('Luas Persegi Panjang: $luas');
+  print('Keliling Persegi Panjang: $keliling');
+
+}
